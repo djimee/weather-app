0225711ec00dc7166db5c9d6f4942bc5a3c48254
Dipraj Jimee <jimeed@outlook.com>
2021-08-18 12:42:21 +0100

    edit forecast response

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location, try another search...', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature
-                + ' degrees. It feels like ' + body.current.feelslike + ' degrees.')
+                + ' degrees. It feels like ' + body.current.feelslike + ' degrees with a humidity of ' + body.current.humiditiy + '%.')
         }
     })
 }
